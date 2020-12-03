ac9a64d7b7accaab26c7d5354bc9e4d6465cad38
unapproachable <tt_zed@hotmail.com>
2020-12-03 14:38:32 -0500

    Refactored to operate on Product instances in order to eliminate the ordinal numbers from the configuration file and iterations. -- Updated template file to demonstrate new format -- Added Product instance concept to codebase that is parsed from a product JSON node -- Updated loops to operate on product list and product instances -- Added future support for per product "check_shipping" and "used" flags -- Updated startup messaging to better indicate what we found in the configuration file and what we're looking for

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -1,4 +1,5 @@
 import json
+from collections import namedtuple
 
 
 def find_values(json_repr, id):
@@ -18,3 +19,17 @@ def find_values(json_repr, id):
 class InvalidAutoBuyConfigException(Exception):
     def __init__(self, message):
         super().__init__(message)
+
+
+def product_loader(product_json):
+    # Get the fields that are included in the JSON configuration from the User
+    fields = list(product_json.keys())
+    # Append any fields required by the code that aren't found in the JSON to the end of the fields
+    known_fields = ["name", "asin_list", "reserve", "check_shipping", "used"]
+    for field in known_fields:
+        if field not in fields:
+            fields.append(field)
+    product = namedtuple(
+        "product", fields, defaults=(None,) * len(product_json.keys())
+    )(*product_json.values())
+    return product
